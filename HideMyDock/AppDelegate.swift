73388f2a38e647125a54e310bdf4fb7f2963023d
a1usha <aleksandr.ushaev@hotmail.com>
2021-11-23 23:51:20 +0700

    Added ability to show/hide dock

--- HideMyDock/AppDelegate.swift
+++ HideMyDock/AppDelegate.swift
@@ -9,30 +9,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     var statusItem: NSStatusItem?
     
+    var enableDockAutoHide = NSAppleScript(source:
+    """
+    tell application "System Events"
+        set autohide of dock preferences to true
+    end tell
+    """
+    )
+    
+    var disableDockAutoHide = NSAppleScript(source:
+    """
+    tell application "System Events"
+        set autohide of dock preferences to false
+    end tell
+    """)
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
         
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         if let menuButton = statusItem?.button {
             menuButton.image = NSImage(systemSymbolName: "icloud.and.arrow.up.fill", accessibilityDescription: nil)
         }
-        
+                
         constructMenu()
+            
     }
     
     func constructMenu() {
         let menu = NSMenu()
         
-        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem(title: "Hide Dock", action: #selector(AppDelegate.hideDock(_:)), keyEquivalent: "H"))
+        menu.addItem(NSMenuItem(title: "Show Dock", action: #selector(AppDelegate.showDock(_:)), keyEquivalent: "S"))
         menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        menu.addItem(NSMenuItem(title: "Quit HideMyDock", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         
         statusItem?.menu = menu
     }
     
-    @objc func printQuote(_ sender: Any?) {
-        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
-        let quoteAuthor = "Mark Twain"
+    @objc func hideDock(_ sender: Any?) {
+        enableDockAutoHide?.executeAndReturnError(nil)
         
-        print("\(quoteText) — \(quoteAuthor)")
+        if let menuButton = statusItem?.button {
+            menuButton.image = NSImage(systemSymbolName: "icloud.and.arrow.down.fill", accessibilityDescription: nil)
+        }
+    }
+    
+    @objc func showDock(_ sender: Any?) {
+        disableDockAutoHide?.executeAndReturnError(nil)
+        
+        if let menuButton = statusItem?.button {
+            menuButton.image = NSImage(systemSymbolName: "icloud.and.arrow.up.fill", accessibilityDescription: nil)
+        }
     }
 }
