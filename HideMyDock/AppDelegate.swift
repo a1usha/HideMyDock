94fa4925e1106b8db7bb6e847f214cc7f0ea2a1c
a1usha <aleksandr.ushaev@hotmail.com>
2021-11-24 12:52:16 +0700

    Testing

--- HideMyDock/AppDelegate.swift
+++ HideMyDock/AppDelegate.swift
@@ -14,8 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     tell application "System Events"
         set autohide of dock preferences to true
     end tell
-    """
-    )
+    """)
     
     var disableDockAutoHide = NSAppleScript(source:
     """
@@ -30,16 +29,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let menuButton = statusItem?.button {
             menuButton.image = NSImage(systemSymbolName: "icloud.and.arrow.up.fill", accessibilityDescription: nil)
         }
-                
+        
         constructMenu()
-            
+        
+//        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(self.spaceChange), name: NSWorkspace.activeSpaceDidChangeNotification, object: nil)
     }
     
     func constructMenu() {
         let menu = NSMenu()
         
-        menu.addItem(NSMenuItem(title: "Hide Dock", action: #selector(AppDelegate.hideDock(_:)), keyEquivalent: "H"))
-        menu.addItem(NSMenuItem(title: "Show Dock", action: #selector(AppDelegate.showDock(_:)), keyEquivalent: "S"))
+        menu.addItem(NSMenuItem(title: "Hide Dock", action: #selector(AppDelegate.spaceChange(_:)), keyEquivalent: "h"))
+        menu.addItem(NSMenuItem(title: "Show Dock", action: #selector(AppDelegate.showDock(_:)), keyEquivalent: "s"))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit HideMyDock", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         
@@ -60,5 +60,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let menuButton = statusItem?.button {
             menuButton.image = NSImage(systemSymbolName: "icloud.and.arrow.up.fill", accessibilityDescription: nil)
         }
+    }
+    
+    @objc func spaceChange(_ sender: Any?) {
+//        enableDockAutoHide?.executeAndReturnError(nil)
+        
+//        do {
+//            try print(safeShell("defaults read com.apple.spaces"))
+//        } catch {
+//
+//        }
+//        let defaults = UserDefaults()
+//        defaults.addSuite(named: <#T##String#>)
+        
+        if let theDefaults = UserDefaults(suiteName: "com.apple.spaces") {
+            theDefaults.synchronize()
+            
+            let spacesDisplayConfiguration = theDefaults.dictionary(forKey: "SpacesDisplayConfiguration")
+            let managementData = spacesDisplayConfiguration?["Management Data"] as! [String : Any?]
+            let monitors = managementData["Monitors"] as! NSArray
+            let currentSpace = (monitors[0] as? [String : Any?])!["Current Space"]
+            
+            
+            print(currentSpace)
+        }
+    }
+    
+    func safeShell(_ command: String) throws -> String {
+        let task = Process()
+        let pipe = Pipe()
+        
+        task.standardOutput = pipe
+        task.standardError = pipe
+        task.arguments = ["-c", command]
+        task.executableURL = URL(fileURLWithPath: "/bin/zsh") //<--updated
+
+        do {
+            try task.run() //<--updated
+        }
+        catch { throw error }
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+        
+        return output
     }
 }
